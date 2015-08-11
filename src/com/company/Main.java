a804c22c2c379fa8399a650ec6e034375402f658
Stas Kirichok <kirichok.stas@gmail.com>
2015-08-12 01:14:15 +0300

    [Refactor] Clean-up project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,41 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Parent parent = new Parent();
-        parent.name = "John";
-        Parent hisSon = new Child();
-        hisSon.name = "Vasya";
-
-        parent.say(parent.name);
-        hisSon.say(hisSon.name);
     }
 
-}
-
-class Parent {
-
-    String name;
-
-    public void say(String whatToSay) {
-        System.out.println(whatToSay);
-    }
-}
-
-class Child extends Parent {
-
-    public void method1() {
-        System.out.println("method1");
-    }
-
-    public static void method2() {
-        System.out.println("method1");
-    }
-
-    @Override
-    public void say(String whatToSay) {
-        super.say(whatToSay);
-        System.out.println("Always say YES");
-    }
 }
 
 
